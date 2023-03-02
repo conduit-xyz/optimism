3d27acb55d5534bec9617bcda9c7ae6a61476346
Andrew Huang <k.andrew.huang@gmail.com>
2023-03-02 10:30:35 -0800

    Allow configuration of block time and gas limit on the devnet

--- op-node/generate-devnet-config.sh
+++ op-node/generate-devnet-config.sh
@@ -2,9 +2,11 @@
 set -eu
 NETWORK=devnetL1
 
-
+GAS_BLOCK_LIMIT="${GAS_BLOCK_LIMIT:-0xE4E1C0}"
+L2_BLOCK_TIME="${L2_BLOCK_TIME:-2}"
 TIMESTAMP=$(date +%s | xargs printf '0x%x')
-cat "devnetL1.json" | jq -r ".l1GenesisBlockTimestamp = \"$TIMESTAMP\"" > /tmp/bedrock-devnet-deploy-config.json
+cat "devnetL1.json" | jq -r ".l1GenesisBlockTimestamp = \"$TIMESTAMP\"" | jq -r ".l2GenesisBlockGasLimit = \"$GAS_BLOCK_LIMIT\"" | jq -r ".l2BlockTime = $L2_BLOCK_TIME" > /tmp/bedrock-devnet-deploy-config.json
+
 (
   op-node genesis devnet \
       --deploy-config /tmp/bedrock-devnet-deploy-config.json \
