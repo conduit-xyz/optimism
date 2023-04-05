e24e023e0e849bf67659446aa7a5d7a239423168
Andrew Huang <k.andrew.huang@gmail.com>
2023-04-05 14:15:31 -0700

    Update default gas block limit

--- op-node/generate-devnet-config.sh
+++ op-node/generate-devnet-config.sh
@@ -2,7 +2,7 @@
 set -eu
 NETWORK=devnetL1
 
-GAS_BLOCK_LIMIT="${GAS_BLOCK_LIMIT:-0xE4E1C0}"
+GAS_BLOCK_LIMIT="${GAS_BLOCK_LIMIT:-0x1c9c380}"
 L2_BLOCK_TIME="${L2_BLOCK_TIME:-2}"
 TIMESTAMP=$(date +%s | xargs printf '0x%x')
 cat "devnetL1.json" | jq -r ".l1GenesisBlockTimestamp = \"$TIMESTAMP\"" | jq -r ".l2GenesisBlockGasLimit = \"$GAS_BLOCK_LIMIT\"" | jq -r ".l2BlockTime = $L2_BLOCK_TIME" > /tmp/bedrock-devnet-deploy-config.json
